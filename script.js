b9631325c65f821dccf5c5674456ee535ba5c502
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-24 23:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function mincost(arr)
 { 
+	arr=[4,3,2,6]
+	let sum=0
+	while(!arr)
+		{
+			arr.sort() 
+			sum+= arr[0] + arr[1]
+			arr.push(arr[0] + arr[1])
+			arr.shift();
+			arr.shift();
+			
+		}
+	
+	//[2,3,4,6]
+	
+	//2+3=5 + 9 + 15
+	//5,4,6
+	//4,5,6
+	//4+5=9
+	
+	//9,6
+	//6,9
+	//15
+	
+	
+	
 //write your code here
 // return the min cost
   
